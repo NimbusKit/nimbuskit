f6e7f98ab978fce162064e6954011a0c00bc5e8b
Jeff Verkoeyen <jverkoey@gmail.com>
2014-02-19 07:43:34 -0400

    Add memorymapping feature.

--- framework/src/NimbusKit.h
+++ framework/src/NimbusKit.h
@@ -26,6 +26,7 @@
 #import <NimbusKit/NimbusInterapp.h>
 #import <NimbusKit/NimbusNetworkImage.h>
 #import <NimbusKit/NimbusOverview.h>
+#import <NimbusKit/NimbusMemoryMapping.h>
 #import <NimbusKit/NimbusPagingScrollView.h>
 #import <NimbusKit/NimbusPhotos.h>
 #import <NimbusKit/NimbusSnapshotRotation.h>
